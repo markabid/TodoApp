15dd8956e26ea5acaa525cf77130006ae8e5c4b1
markabid <markabid@buffalo.edu>
2018-02-12 11:41:34 -0500

    items can be added and deleted

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,25 +1,13 @@
 import { Component, ElementRef, OnInit, ViewChild, NgModule } from "@angular/core";
 import { TNSCheckBoxModule } from 'nativescript-checkbox/angular';
 import { TextField } from "ui/text-field";
+import { Todo } from "../../shared/todo/todo";
+import * as ApplicationSettings from "application-settings";
 
 @NgModule({
     imports: [TNSCheckBoxModule],
     // etc.
 })
-export class YourModule {}
- 
-// component:
-export class SomeComponent  {
-    @ViewChild("CB1") FirstCheckBox: ElementRef;
-    constructor() {}
-    public toggleCheck() {
-        this.FirstCheckBox.nativeElement.toggle();
-    }
- 
-    public getCheckProp() {
-        console.log('checked prop value = ' + this.FirstCheckBox.nativeElement.checked);
-    }
-}
 
 @Component({
   selector: "list",
@@ -27,25 +15,22 @@ export class SomeComponent  {
   templateUrl: "./list.html",
   styleUrls: ["./list-common.css", "./list.css"]
 })
+
 export class ListComponent implements OnInit {
-  todoList: Array<Object> = [];
+  todoList: Array<any>;
   todo = "";
+  id; 
   @ViewChild("todoTextField") todoTextField: ElementRef;
+  @ViewChild("CB1") FirstCheckBox: ElementRef;
 
   ngOnInit() {
-    this.todoList.push({ name: "Apples" });
-    this.todoList.push({ name: "Bananas" });
-    this.todoList.push({ name: "Oranges" });
-    this.todoList.push({ name: "Apples" });
-    this.todoList.push({ name: "Bananas" });
-    this.todoList.push({ name: "Oranges" });
-    this.todoList.push({ name: "Apples" });
-    this.todoList.push({ name: "Bananas" });
-    this.todoList.push({ name: "Oranges" });
+    this.todoList = JSON.parse(ApplicationSettings.getString("todo", "[]"));
+    this.id = ApplicationSettings.getNumber("id",0);
   }
 
-  delete(){
-      
+  delete(id){
+    this.todoList.splice(this.todoList.findIndex(x => x.id == id), 1);
+    ApplicationSettings.setString("todo", JSON.stringify(this.todoList));
   }
 
   add(){
@@ -57,5 +42,59 @@ export class ListComponent implements OnInit {
     // Dismiss the keyboard
     let textField = <TextField>this.todoTextField.nativeElement;
     textField.dismissSoftInput();
+    textField.text = "";
+
+    const todoItem = {
+        todoText: this.todo,
+        isDone: false,
+        dateCreated: new Date().toISOString().substring(0, 10),
+        id: this.id + 1
+    }
+
+    this.todoList.unshift(todoItem);
+    ApplicationSettings.setString("todo", JSON.stringify(this.todoList));
+    this.id++;
+    ApplicationSettings.setNumber("id", this.id);
+    console.log(this.id+1);
+  }
+
+  findItem(element, id) {
+    return element.id == id;
+  }
+
+  check(text, date, id, curBool){
+    var temp = false;
+    if(!curBool){
+        temp = true; 
+    }
+
+    const item = {
+        todoText: text,
+        isDone: temp,
+        dateCreated: date,
+        id: id
+    }
+
+    this.todoList.splice(this.todoList.findIndex(x => x.id == item.id), 1, item);
+    //this.todoList[this.todoList.findIndex(x => x.id == item.id)] = item;
+    ApplicationSettings.setString("todo", JSON.stringify(this.todoList));
+  }
+
+  setDate(date){
+    const diff =  Math.floor(( Date.parse(date) - Date.parse(new Date().toISOString().substring(0, 10)) ) / 86400000); 
+    switch (true) {
+        case (diff < 1):
+            return "added today";
+        case (diff == 1):
+            return "added yesterday";
+        case (diff < 30):
+            return "added " + diff + " days ago";
+        case (diff < 60):
+            return "added a month ago"
+        case (diff < 365):
+            return "added " + Math.floor(diff/30) + " months ago"; 
+        case(diff >= 365):
+            return "added over a year ago";
+    }
   }
 }
---
+++ app/shared/todo/todo.ts
@@ -0,0 +1,5 @@
+export class Todo{
+    constructor(public item: string, public isDone: boolean, public dateCreated : Date){
+
+    }
+}
